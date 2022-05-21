91ee9a984fb82a4e60be9547a23e4aee6b4b1ad6
Juan Esteban Oviedo <joviedog@unal.edu.co>
2022-05-20 22:53:30 -0500

    Fix volunteer serializing on unrelated petition

--- search/serializers.py
+++ search/serializers.py
@@ -1,13 +1,15 @@
 from rest_framework import serializers
 from database.models import Session, CustomUser
+from donations.serializers import CustomUserSerializer
 
 
 class SessionSerializer(serializers.ModelSerializer):
+    volunteer = CustomUserSerializer
     class Meta:
         model = Session
-        fields = '__all__'
+        fields = ['name', 'date', 'start_time', 'end_time', 'description', 'category', 'organization']
 
-# Organization Serializer
+
 class OrganizationSerializer(serializers.ModelSerializer):
 
     class Meta:
