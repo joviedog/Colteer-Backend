36bea49b4156dd36997be68eef89fb0981fc6a75
CarlosJulian1231 <ccordero@unal.edu.co>
2022-05-24 23:38:39 -0500

    Correccion Registro de usuarios en sesiones

--- volunteer/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import URLPattern, path, include
-from volunteer.api import *
-
-urlpatterns = [
-    path('api/volunteer/register-volunteer', api_register_volunteer, name = "api_register_volunteer"),
-    
-]
